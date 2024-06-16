ea76bcd7e8e0e8e47d0ae4309e297b214989b498
lh3980 <69008447+lh3980@users.noreply.github.com>
2024-06-16 20:05:42 +0800

    Update AwemeContactPhotoProvider.h

--- AwemeContactPhotoProvider.h
+++ AwemeContactPhotoProvider.h
@@ -1,6 +1,5 @@
 #import "ShortLook-API.h"
 
 @interface AwemeContactPhotoProvider : NSObject <DDNotificationContactPhotoProviding>
-
 - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification;
 @end
