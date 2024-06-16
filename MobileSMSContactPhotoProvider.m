20f2c476e3e3905c86cf1143dbfc9ad979c4e38d
lh3980 <69008447+lh3980@users.noreply.github.com>
2024-06-17 01:33:57 +0800

    Update MobileSMSContactPhotoProvider.m

--- MobileSMSContactPhotoProvider.m
+++ MobileSMSContactPhotoProvider.m
@@ -3,11 +3,23 @@
 @implementation MobileSMSContactPhotoProvider
 
 - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification {
-	NSString *attachmentString = [notification.applicationUserInfo valueForKeyPath:@"attachment"];
-    if (!attachmentString) return nil;
-    NSURL *attachmentURL = [NSURL URLWithString:attachmentString];
-    if (!attachmentURL) return nil;
-    return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:attachmentString fromURL:attachmentURL];
+    UIImage *image = [UIImage imageWithContentsOfFile:@"/var/mobile/Library/Application Support/Dynastic/ShortLook/icon.png"];
+    // There are two ways to return an image. Asynchronously (if it might take a while) and synchronously (instant).
+    // Read the docs for more information.
+
+    // Asynchronous method:
+    DDNotificationContactPhotoPromiseOffer *offer = [[NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") alloc] initWithPhotoIdentifier:@"fantastic-unique-identifier"];
+    [offer fulfillWithBlock:^(DDNotificationContactPhotoPromise *promise) {
+        promise.backgroundColor = [UIColor yellowColor]; // you can optionally provide a background colour in the case of partially transparent images
+
+        [NSThread sleepForTimeInterval:0.5]; // wait half a second to simulate server call
+        [promise resolveWithImage:image];
+        // if failed? -> [promise reject];
+    }];
+    return offer;
+
+    // Synchronous method:
+    return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerInstantlyResolvingPromiseWithPhotoIdentifier:@"super-unique-identifier" image:image];
 }
 
 @end
